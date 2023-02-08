1cd53ee9f2642678bb803df0cc1357b2dac48c4e
Patryk Bański <60692722+PBA77@users.noreply.github.com>
2023-02-08 11:58:54 +0100

    fix example

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,3 @@
 import streamlit as st
 
-if st.experimental_user.email:
-    # Get the user's name from the database.
-    name = get_name_from_db(st.experimental_user.email)
-    st.write('Hello, %s!' % name)
+st.write('Hello, %s!' % st.experimental_user.email)
