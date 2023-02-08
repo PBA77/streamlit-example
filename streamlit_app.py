1d0135a3fd4bdd0e2848f9cbd5f9fba9239568ee
Patryk Bański <60692722+PBA77@users.noreply.github.com>
2023-02-08 11:53:30 +0100

    add user hello

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,10 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
+if st.experimental_user.email:
+    # Get the user's name from the database.
+    name = get_name_from_db(st.experimental_user.email)
+    st.write('Hello, %s!' % name)
 
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
